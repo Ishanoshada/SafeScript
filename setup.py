c944e44db0b6f7166ce6a97f8f410ab694d4e336
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-10-26 07:05:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='safescript',
-    version='1.0.2',
+    version='1.0.3',
     description='A Python module for enhancing web application security',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
